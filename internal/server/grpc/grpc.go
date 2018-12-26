28267114c69c0e236f5468e4b627ae1aca9532eb
Satyajit <emailtovamos@gmail.com>
2018-12-26 10:00:25 +0530

    The struct which will implement the rpc function defined

--- internal/server/grpc/grpc.go
+++ internal/server/grpc/grpc.go
@@ -1,2 +1,17 @@
 package grpc
 
+import (
+	"google.golang.org/grpc"
+)
+
+type Grpc struct {
+	address string
+	srv     *grpc.Server
+}
+
+// NewServer creates a new instance of a gRPC server
+func NewServer(address string) *Grpc {
+	return &Grpc{
+		address: address,
+	}
+}
